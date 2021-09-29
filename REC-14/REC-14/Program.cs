1378c2233a335b6c9cffa4b4a056fcfda5c0ce5a
STokarew <Sseregatokarew@gmail.com>
2021-09-29 18:49:05 +0700

    29.09.2021

---
+++ REC-14/REC-14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace REC_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите порядковый номер числа фибоначчи");
+            int number = Convert.ToInt32(Console.ReadLine());
+            {
+                double a = 1;
+                double b = 1;
+                double c = 0;
+
+                int j = 2;
+                while (j <= number)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                    j++;
+                }
+                Console.WriteLine("Под номером " + number + " в ряде Фибоначчи стоит число " + a);
+                Console.Read();
+            }
+        }
+    }
+}
