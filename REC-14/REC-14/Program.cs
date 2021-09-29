0c3bbceefe6bf351c984b9838ffe96efaeb72317
STokarew <Sseregatokarew@gmail.com>
2021-09-29 18:51:11 +0700

    29.09.2021 2

--- REC-14/REC-14/Program.cs
+++ REC-14/REC-14/Program.cs
@@ -7,21 +7,21 @@ namespace REC_14
         static void Main(string[] args)
         {
             Console.WriteLine("Введите порядковый номер числа фибоначчи");
-            int number = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
             {
                 double a = 1;
                 double b = 1;
                 double c = 0;
 
                 int j = 2;
-                while (j <= number)
+                while (j <= n)
                 {
                     c = a + b;
                     a = b;
                     b = c;
                     j++;
                 }
-                Console.WriteLine("Под номером " + number + " в ряде Фибоначчи стоит число " + a);
+                Console.WriteLine("Под номером " + n + " в ряде Фибоначчи стоит число " + a);
                 Console.Read();
             }
         }
